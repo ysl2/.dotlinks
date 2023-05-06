e2d6fdf7f2c5f6e3ce26a1692ec06f79cd11b1a2
ysl2 <www.songli.yu@gmail.com>
2023-05-06 20:53:13 +0800

    Add windows terminal config; Update pwsh config.

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,34 @@
+# Ref: https://github.com/gokcehan/lf/blob/master/etc/lf.ps1
+# Change working dir in powershell to last dir in lf on exit.
+#
+# You need to put this file to a folder in $ENV:PATH variable.
+#
+# You may also like to assign a key to this command:
+#
+#     Set-PSReadLineKeyHandler -Chord Ctrl+o -ScriptBlock {
+#         [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+#         [Microsoft.PowerShell.PSConsoleReadLine]::Insert('lfcd.ps1')
+#         [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+#     }
+#
+# You may put this in one of the profiles found in $PROFILE.
+#
+
+function lf {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    & "C:\ProgramData\chocolatey\bin\lf.exe" -last-dir-path="$tmp" $args
+    if (Test-Path -PathType Leaf "$tmp") {
+        $dir = Get-Content "$tmp"
+        Remove-Item -Force "$tmp"
+        if (Test-Path -PathType Container "$dir") {
+            if ("$dir" -ne "$pwd") {
+                cd "$dir"
+            }
+        }
+    }
+}
+
+Set-Alias lg lazygit
+
 Invoke-Expression (&starship init powershell)
 # Set-Location $HOMEPATH
