1d67a2169f431bec0a26250208d12e353bbe241d
songliyu <songliyu@tencent.com>
2024-11-17 22:48:05 +0800

    Fix zero messages.
    Fix

--- .config/sketchybar/plugins/app.sh
+++ .config/sketchybar/plugins/app.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Ref: https://github.com/AIboy996/dotfiles/blob/main/sketchybar/items/wechat.sh
 
 
 main() {
@@ -6,10 +7,12 @@ main() {
 	label="$(lsappinfo -all list | grep "$1" | grep -Eo "\"StatusLabel\"=\{ \"label\"=\"?(.*?)\"? \}" | sed 's/\"StatusLabel\"={ \"label\"=\(.*\) }/\1/g')"
 
 	if [[ ! "$label" =~ ^\".*\"$ ]]; then
+        sketchybar --set "$NAME" label=0
         return
     fi
     label="$(echo "$label" | sed 's/^"//' | sed 's/"$//')"
     if [ -z "$label" ]; then
+        sketchybar --set "$NAME" label=0
         return
     fi
     sketchybar --set "$NAME" label="$label"
