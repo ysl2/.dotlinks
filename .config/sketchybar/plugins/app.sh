679cf1611d3f6c0dd489bb68cafd0e7eda1127df
songliyu <songliyu@tencent.com>
2024-11-19 23:41:43 +0800

    Fix wecom display for sketchybar.

--- .config/sketchybar/plugins/app.sh
+++ .config/sketchybar/plugins/app.sh
@@ -7,15 +7,15 @@ main() {
 	label="$(lsappinfo -all list | grep "$1" | grep -Eo "\"StatusLabel\"=\{ \"label\"=\"?(.*?)\"? \}" | sed 's/\"StatusLabel\"={ \"label\"=\(.*\) }/\1/g')"
 
 	if [[ ! "$label" =~ ^\".*\"$ ]]; then
-        sketchybar --set "$NAME" label=0
+        sketchybar --set "$NAME" label=0 display=0
         return
     fi
     label="$(echo "$label" | sed 's/^"//' | sed 's/"$//')"
     if [ -z "$label" ]; then
-        sketchybar --set "$NAME" label=0
+        sketchybar --set "$NAME" label=0 display=active
         return
     fi
-    sketchybar --set "$NAME" label="$label"
+    sketchybar --set "$NAME" label="$label" display=active
 }
 
 
