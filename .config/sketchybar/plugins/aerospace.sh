324341a930cf73c7e6f4bcc0d095b11ee39c8718
songliyu <songliyu@tencent.com>
2025-02-28 21:38:21 +0800

    Speed up the aerospace workspace switch.

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -5,18 +5,23 @@ main() {
     local i="$1"
     local m="$2"
 
-    if [ "$i" = "$(aerospace list-workspaces --focused)" ]; then
-        sketchybar --set "$NAME" background.drawing=on display="$m"
-        return
+    # if [ "$i" = "$(aerospace list-workspaces --focused)" ]; then
+    # Highlight the focused workspace.
+    if [ "$i" = "$FOCUSED_WORKSPACE" ]; then
+        sketchybar --set "$NAME" background.drawing=on display="$m" &
+    else
+        sketchybar --set "$NAME" background.drawing=off &
+        # If workspace has window, show it.
+        if [ -n "$(aerospace list-windows --workspace "$i")" ]; then
+            sketchybar --set "$NAME" display="$m" &
+        else
+            # Else, hide it.
+            sketchybar --set "$NAME" display=0 &
+        fi
     fi
-    if [ -n "$(aerospace list-windows --workspace "$i")" ]; then
-        sketchybar --set "$NAME" background.drawing=off display="$m"
-        return
-    fi
-    sketchybar --set "$NAME" background.drawing=off display=0
 }
 
 
-if [ "$SENDER" = 'aerospace_workspace_change' ]; then
+if [ "$SENDER" = aerospace_workspace_change ]; then
     main "$@"
 fi
