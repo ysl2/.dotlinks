039401e2f9ef87af203031013db8f38ef1e13fed
songliyu <songliyu@tencent.com>
2025-03-24 20:12:12 +0800

    Format sketchybar

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -7,22 +7,11 @@ if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
-case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
-  ;;
-  [6-8][0-9]) ICON=""
-  ;;
-  [3-5][0-9]) ICON=""
-  ;;
-  [1-2][0-9]) ICON=""
-  ;;
-  *) ICON=""
-esac
-
 if [[ "$CHARGING" != "" ]]; then
-  ICON=""
+  # CHARGING='(Charging)'
+  CHARGING='+'
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set "$NAME" icon="Bat:" label="${PERCENTAGE}% ${CHARGING}"
--- .config/sketchybar/plugins/volume.sh
+++ .config/sketchybar/plugins/volume.sh
@@ -4,21 +4,9 @@
 # percentage is passed to the script.
 
 main() {
-  VOLUME="$INFO"
-
-  case "$VOLUME" in
-    [6-9][0-9]|100) ICON="󰕾"
-    ;;
-    [3-5][0-9]) ICON="󰖀"
-    ;;
-    [1-9]|[1-2][0-9]) ICON="󰕿"
-    ;;
-    *) ICON="󰖁"
-  esac
-
-  sketchybar --set "$NAME" icon="$ICON" label="$VOLUME% ($(SwitchAudioSource -c))"
+  sketchybar --set "$NAME" icon="Vol:" label="$INFO% ($(SwitchAudioSource -c))"
 }
 
-if [ "$SENDER" = 'volume_change' ]; then
+if [ "$SENDER" = volume_change ]; then
     main "$@"
 fi
